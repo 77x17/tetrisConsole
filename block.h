e0a020dfcb46cbf759c1e71f43c04fece7611db2
HQH <86921256+CuHuy09@users.noreply.github.com>
2023-09-14 22:09:22 +0700

    Add files via upload

--- block.h
+++ block.h
@@ -1,3 +1,4 @@
+#define sz(s) (int)s.size()
 using matrix = vector<vector<int>>;
 
 int random(int l, int r) {
@@ -6,6 +7,23 @@ int random(int l, int r) {
 
 struct Block{
     matrix arr;
+
+    int get_size() { return sz(arr); }
+
+    void update_layer(int p, int n) {
+        matrix temp = arr;
+        for (int i = p; i < n - p; i++) {
+            arr[p][i] = temp[i][n - p - 1];
+            arr[i][n - p - 1] = temp[n - p - 1][n - i - 1];
+            arr[n - p - 1][n - i - 1] = temp[n - i - 1][p];
+            arr[n - i - 1][p] = temp[p][i];
+        }
+    }
+    void rotate() {
+        int n = get_size();
+        for (int i = 0; i < n / 2; i++) 
+            update_layer(i, n);
+    }
 };
 
 Block blockArr[2];
@@ -16,10 +34,10 @@ Block randBlock() {
     blockArr[0].arr = {{1, 1},
                        {1, 1}};
 
-    blockArr[1].arr = {{0, 1, 0, 0},
-                       {0, 1, 0, 0},
-                       {0, 1, 0, 0},
-                       {0, 1, 0, 0}};
+    blockArr[1].arr = {{0, 0, 1, 0},
+                       {0, 0, 1, 0},
+                       {0, 0, 1, 0},
+                       {0, 0, 1, 0}};
 
     return blockArr[random(0, 1)];
 }
